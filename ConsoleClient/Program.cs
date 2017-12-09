220ca35dbceb12df8b4b6aa0dcdbd7419ef94cf9
giansalex <giansalex@gmail.com>
2017-12-09 11:20:12 -0500

    add console client

---
+++ ConsoleClient/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.AspNetCore.Sockets;
+
+namespace ConsoleClient
+{
+    class Program
+    {
+        private static HubConnection _connection;
+        static void Main(string[] args)
+        {
+            StartConnectionAsync().Wait();
+            _connection.On<string>("Send", (message) =>
+            {
+                Console.WriteLine($"said: {message}");
+            });
+
+            Console.ReadLine();
+            DisposeAsync().Wait();
+        }
+
+        public static async Task StartConnectionAsync()
+        {
+            _connection = new HubConnectionBuilder()
+                 .WithUrl("http://localhost:64988/loopy")
+                 .WithConsoleLogger()
+                 .Build();
+
+
+            await _connection.StartAsync();
+        }
+
+        public static async Task DisposeAsync()
+        {
+            await _connection.DisposeAsync();
+        }
+    }
+}
